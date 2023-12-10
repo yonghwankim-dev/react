6c630517d13b271f6bb9596e0156ead6dad55765
yonghwankim-dev <dragonbead95@naver.com>
2023-12-10 18:19:39 +0900

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import TOC from './components/TOC'
+import Content from './components/Content';
+import Subject from './components/Subject';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{
+        title: 'WEB',
+        sub: 'world wide web!',
+      },
+      contents:[
+        {
+          id: 1,
+          title: 'HTML',
+          desc: 'HTML is HyperText'
+        },
+        {
+          id: 2,
+          title: 'CSS',
+          desc: 'CSS is for design'
+        },
+        {
+          id: 3,
+          title: 'JavaScript',
+          desc: 'JavaScript is for interactive'
+        }
+      ]
+    }
+  }
+
+  render (){
+    return(
+      <div className="App">
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        <Subject title="React" sub="For UI"></Subject>
+        <TOC data={this.state.contents}></TOC>
+
+        <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
+      </div>
+    )
+  }
 }
 
 export default App;
